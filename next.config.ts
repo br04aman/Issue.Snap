0dfa1929426c78fd50d7ff85f01ec00114555776
br04aman <162725210+br04aman@users.noreply.github.com>
2025-12-31 19:04:20 +0530

    next.onfig update1

--- next.config.ts
+++ next.config.ts
@@ -1,7 +1,10 @@
-import type {NextConfig} from 'next';
+import type { NextConfig } from 'next';
 
 const nextConfig: NextConfig = {
   /* config options here */
+  serverActions: {
+    bodySizeLimit: '1mb',
+  },
   typescript: {
     ignoreBuildErrors: true,
   },
